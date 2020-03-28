c12cea830e4ead001d3ebf9b90ee68808719ea7b
dGrammatiko <d.grammatiko@gmail.com>
2020-03-28 13:16:02 +0100

    compress js

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,13 @@
 import resolve from 'rollup-plugin-node-resolve';
+import { terser } from "rollup-plugin-terser";
+import minifyHTML from 'rollup-plugin-minify-html-literals';
+
 export default {
   input: 'src_site/js/data.src.js',
   plugins: [
     resolve(),
+    minifyHTML(),
+    terser(),
   ],
   context: 'null',
   moduleContext: 'null',
